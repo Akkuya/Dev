74365f36017cd83e32f3d88f459cdefd0c9a5d58
Rishi <satyajitdatta03@gmail.com>
2025-09-23 00:26:30 -0400

    first commit

---
+++ a1_starter/fantasy_draft_functions.py
@@ -0,0 +1,135 @@
+"""CSCA08: Fall 2025 -- Assignment 1: Ice Hockey Fantasy Draft
+
+This code is provided solely for the personal and private use of
+students taking the CSCA08 course at the University of
+Toronto. Copying for purposes other than this use is expressly
+prohibited. All forms of distribution of this code, whether as given
+or with any changes, are expressly prohibited.
+
+All of the files in this directory and all subdirectories are:
+Copyright (c) 2020-2025 Mario Badr, Jennifer Campbell, Tom Fairgrieve,
+Diane Horton, Michael Liut, Jacqueline Smith, and Anya Tafliovich.
+
+"""
+
+from constants import (
+    POINTS_PER_GOAL,
+    POINTS_PER_ASSIST,
+    POINTS_PER_HIT,
+    F_DCS_PER_POINT,
+    D_DCS_PER_POINT,
+    FORWARDS_NEEDED,
+    DEFENCEMEN_NEEDED,
+    GOALIES_NEEDED,
+    BUDGET,
+    PLAYERS_TO_SELECT,
+    FORWARD,
+    DEFENCEMEN,
+    GOALIE,
+    SV_VALUE,
+    GAA_VALUE,
+)
+
+
+# provided
+def get_player_id(player: str) -> str:
+    """Return the id of player if the string is non-empty;
+    otherwise return the empty string.
+
+    Precondition: player is the string of player stats as
+    seen in players.txt
+
+    >>> get_player_id('MGO_PD_G0-_A14_DC43_H70_Pr5-')
+    'MGO'
+    >>> get_player_id('NSH_PF_G7-_A14_DC20_H73_Pr10')
+    'NSH'
+    >>> get_player_id('')
+    ''
+
+    """
+    if len(player) == 0:
+        return ''
+    return player[:3]
+
+def get_price(player: str) -> int:
+
+    """Return the price of player
+       #TODO 
+    """
+    if len(player) == 0:
+        return 0
+    price = player[-2:]
+    if price[1] == "-":
+        return int(price[0])
+    return int(price)
+    
+def can_afford(budget: int, player: str) -> bool:
+    """Return True if and only if budget is at least the price of player.
+
+    Precondition: budget >= 0, player is the string of player stats
+                  as seen in players.txt
+
+    >>> can_afford(10, 'MGO_PD_G0-_A14_DC43_H70_Pr5-')
+    True
+    >>> can_afford(4, 'NSH_PF_G7-_A14_DC20_H73_Pr10')
+    False
+    >>> can_afford(0, '')
+    False
+
+    """
+    if len(player) == 0:
+        return False
+    return budget >= get_price(player)
+
+# provided as example of calling another function as helper
+def is_player_available(player: str, players_available: str) -> bool:
+
+    """Return True if and only if the id of player is in players_available.
+
+    Precondition: player is the string of player stats
+                  as seen in players.txt,
+                  players_available is the string of player ids
+                  that are currently available for selection seperated
+                  by _.
+
+    >>> is_player_available('MGO_PD_G0-_A14_DC43_H70_Pr5-', 'DOL_NCA_MGO_AHS_')
+    True
+    >>> is_player_available('GGG_PD_G0-_A14_DC43_H70_Pr5-', 'DOL_NCA_MGO_AHS_')
+    False
+    >>> is_player_available('', 'DOL_NCA_MGO_AHS_')
+    False
+
+    """
+
+    if len(player) == 0:
+        return False
+    return get_player_id(player) in players_available
+
+
+# provided
+def get_position(player: str) -> str:
+    """Return the position of player if player is non-empty;
+    otherwise return the empty string.
+
+    Precondition: player is the string of player stats
+    as seen in players.txt
+
+    >>> get_position('MGO_PD_G0-_A14_DC43_H70_Pr5-')
+    'D'
+    >>> get_position('NSH_PF_G7-_A14_DC20_H73_Pr10')
+    'F'
+    >>> get_position('CLA_PG_GAA2.23_SV0.910_Pr20')
+    'G'
+    >>> get_position('')
+    ''
+
+    """
+
+    if len(player) == 0:
+        return ""
+    return player[5]
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
---
+++ a1_starter/test.py
@@ -0,0 +1,2 @@
+x="CHO_PF_G25_A29_DC11_H40_Pr20"
+print(x[-2:])
