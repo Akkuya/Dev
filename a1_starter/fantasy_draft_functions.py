d366a70b2f74b58023ce1230acfea17e495db064
Rishi <satyajitdatta03@gmail.com>
2025-10-01 01:22:18 -0400

    docstrings finished

--- a1_starter/fantasy_draft_functions.py
+++ a1_starter/fantasy_draft_functions.py
@@ -51,12 +51,13 @@ def get_player_id(player: str) -> str:
         return ''
     return player[:3]
 
+
 def get_price(player: str) -> int:
 
     """Return the price of player if the string is non-empty;
     otherwise return 0.
 
-    Precondition: player is the string of player stats as 
+    Precondition: player is the string of player stats as
     seen in players.txt
 
     >>> get_price('MGO_PD_G0-_A14_DC43_H70_Pr5-')
@@ -65,7 +66,7 @@ def get_price(player: str) -> int:
     10
     >>> get_price('')
     0
-    
+
     """
     if len(player) == 0:
         return 0
@@ -73,7 +74,8 @@ def get_price(player: str) -> int:
     if price[1] == "-":
         return int(price[0])
     return int(price)
-  
+
+
 # provided
 def get_position(player: str) -> str:
     """Return the position of player if player is non-empty;
@@ -96,6 +98,7 @@ def get_position(player: str) -> str:
     if len(player) == 0:
         return ''
     return player[5]
+
 
 # provided as example of calling another function as helper
 def is_player_available(player: str, players_available: str) -> bool:
@@ -120,27 +123,45 @@ def is_player_available(player: str, players_available: str) -> bool:
         return False
     return get_player_id(player) in players_available
 
-def can_select(player: str, num_forwards: int, num_defencemen: int, num_goalies: int) -> bool:
-    """Return True if and only the position of player is less than the needed amount of 
-    players with that same position. If player is the empty string, return True.
+
+def can_select(player: str,
+               num_forwards: int,
+               num_defencemen: int,
+               num_goalies: int) -> bool:
+    """Return True if and only if the number of players with the same
+    position of player is less than the needed amount of players with the
+    same position.
+    If the player is a forward and num_forwards < FORWARDS_NEEDED, return True.
+    If the player is a defencemen and num_defencemen < DEFENCEMEN_NEEDED, 
+    return True.
+    If the player is a goalie and num_goalies < GOALIES_NEEDED, return True.
+    If player is the empty string, return True.
+
+    Precondition: player is the string of player stats
+                  as seen in players.txt,
 
 
+    >>> can_select('GGG_PD_G0-_A14_DC43_H70_Pr5-', 0, 0, 0)
+    True
+    >>> can_select('GGG_PD_G0-_A14_DC43_H70_Pr5-', 5, 5, 5)
+    False
+    >>> can_select('', 0, 0, 0)
+    True
 
     """
-    
-    
     if len(player) == 0:
         return True
 
     position = get_position(player)
-    if position == 'F' and (num_forwards < FORWARDS_NEEDED):
+    if position == FORWARD and (num_forwards < FORWARDS_NEEDED):
         return True
-    elif position == 'D' and (num_defencemen < DEFENCEMEN_NEEDED):
+    if position == DEFENCEMEN and (num_defencemen < DEFENCEMEN_NEEDED):
         return True
-    elif position == 'G' and (num_goalies < GOALIES_NEEDED):
+    if position == GOALIE and (num_goalies < GOALIES_NEEDED):
         return True
-    
+
     return False
+
 
 def can_afford(budget: int, player: str) -> bool:
     """Return True if and only if budget is at least the price of player.
@@ -160,28 +181,35 @@ def can_afford(budget: int, player: str) -> bool:
         return False
     return budget >= get_price(player)
 
+
 def update_budget(budget: int, player: str) -> int:
-    """Return the updated budget after subtracting the price of player from budget.
-    If player string is empty, return budget without changes.
+    """Return the updated budget after subtracting the price of
+    player from budget. If player string is empty, return
+    budget without changes.
 
     Precondition: player is in the string of player stats as seen in players.txt
 
     >>> update_budget(50, 'MGO_PD_G0-_A14_DC43_H70_Pr5-')
     45
     >>> update_budget(5, 'NSH_PF_G7-_A14_DC20_H73_Pr10')
-    -10
-    >>> update_budget(5, ''):
+    -5
+    >>> update_budget(5, '')
     5
-    """ 
-    return budget - int(get_price(player))     
+
+    """
+    return budget - int(get_price(player))
 
 
 def add_to_team(player: str, player_ids: str) -> str:
     """Return the updated string of player_ids selected by GM, after adding
-    the ID of player. If player already exists in player_ids, return the 
+    the ID of player. If player already exists in player_ids, return the
     unchanged string of player_ids.
 
-    Precondition: player is in the string of player stats as seen in players.txt
+    Precondition: player is the string of player stats
+                  as seen in players.txt,
+                  players_available is the string of player ids
+                  that are currently available for selection seperated
+                  by _.
 
     >>> add_to_team('NSH_PF_G7-_A14_DC20_H73_Pr10', 'MGO_')
     'MGO_NSH_'
@@ -199,88 +227,160 @@ def add_to_team(player: str, player_ids: str) -> str:
     chosen_id = get_player_id(player)
     if chosen_id in player_ids:
         return player_ids
-    return player_ids+chosen_id+'_'
-    
+    return player_ids + chosen_id + '_'
+
+
 def remove_player(player_ids: str, player_index: int) -> str:
     """Returns the updated string of all drafted players, after removing
-    the id preceding the seperator at index player_index from player_ids. 
-    If the characterat player_index is not a seperator, or player_index 
+    the id preceding the seperator at index player_index from player_ids.
+    If the characterat player_index is not a seperator, or player_index
     is negative, then return the unmodified string of player_ids.
 
+    Precondition: players_available is the string of player ids
+                  that are currently drafted for selection seperated
+                  by _.
+
     >>> remove_player('DOL_NCA_MGO_AHS_', 7)
-    'OOL_MGO_AHS_'
+    'DOL_MGO_AHS_'
     >>> remove_player('DOL_NCA_MGO_AHS_', 8)
     'DOL_NCA_MGO_AHS_'
     >>> remove_player('DOL_NCA_MGO_AHS_', -1)
     'DOL_NCA_MGO_AHS_'
-    
+
     """
     if player_ids[player_index] != '_' or player_index < 0:
         return player_ids
-    return player_ids[:player_index-3] + player_ids[player_index+1:]
-    
+    return player_ids[:player_index - 3] + player_ids[player_index + 1:]
+
 
 def compute_dc_points(player: str) -> int:
-    """Return the defensive contribution points of player.  If the player
+    """Return the defensive contribution points of player. If the player
     is a goalie or an empty string, return 0.
 
     Precondition: player is in the string of player stats as seen in players.txt
 
     >>> compute_dc_points('NSH_PF_G7-_A14_DC20_H73_Pr10')
-    
+    2
+    >>> compute_dc_points('MGO_PD_G0-_A14_DC43_H70_Pr5-')
+    8
+    >>> compute_dc_points('CLA_PG_GAA2.23_SV0.910_Pr20')
+    0
+    >>> compute_dc_points('')
+    0
+
     """
     position = get_position(player)
-    if position == 'G' or player == '':
+    if position == GOALIE or player == '':
         return 0
-    
+
     points = 0
     dc = player[17:19]
     if dc[1] == '-':
         dc = dc[0]
-    if position == 'F':
-        points = int(dc) % F_DCS_PER_POINT
-    elif position == 'D':
-        points = int(dc) % D_DCS_PER_POINT
+    if position == FORWARD:
+        points = int(dc) // F_DCS_PER_POINT
+    elif position == DEFENCEMEN:
+        points = int(dc) // D_DCS_PER_POINT
 
     return points
 
+
 def compute_goal_points(player: str) -> int:
     """
-    DOCSTRING
+    Return the goal points of player. If the player is a goalie
+    or an empty string, return 0.
+
+    Precondition: player is in the string of player stats as seen in players.txt
+
+    >>> compute_goal_points('NSH_PF_G7-_A14_DC20_H73_Pr10')
+    28
+    >>> compute_goal_points('CLA_PG_GAA2.23_SV0.910_Pr20')
+    0
+    >>> compute_goal_points('')
+    0
+
     """
-    if get_position(player) == 'G' or len(player) == 0:
+    if get_position(player) == GOALIE or len(player) == 0:
         return 0
     goals = player[8:10]
     if goals[1] == '-':
         goals = goals[0]
     return int(goals) * POINTS_PER_GOAL
 
+
 def compute_assist_points(player: str) -> int:
-    if get_position(player) == 'G' or len(player) == 0:
+    """Return the assist points of player. If the player is a goalie
+    or an empty string, return 0.
+
+    Precondition: player is in the string of player stats as seen in players.txt
+
+    >>> compute_assist_points('NSH_PF_G7-_A14_DC20_H73_Pr10')
+    28
+    >>> compute_assist_points('CLA_PG_GAA2.23_SV0.910_Pr20')
+    0
+    >>> compute_assist_points('')
+    0
+
+    """
+    if get_position(player) == GOALIE or len(player) == 0:
         return 0
     assists = player[12:14]
     if assists[1] == '-':
         assists = assists[0]
     return int(assists) * POINTS_PER_ASSIST
 
+
 def compute_hit_points(player: str) -> float:
-    if get_position(player) == 'G' or len(player) == 0:
+    """Return the hit points of player. If the player is a goalie
+    or an empty string, return 0.
+
+    Precondition: player is in the string of player stats as seen in players.txt
+
+    >>> compute_hit_points('NSH_PF_G7-_A14_DC20_H73_Pr10')
+    18.25
+    >>> compute_hit_points('MGO_PD_G0-_A14_DC43_H70_Pr5-')
+    17.5
+    >>> compute_hit_points('CLA_PG_GAA2.23_SV0.910_Pr20')
+    0
+
+    """
+    if get_position(player) == GOALIE or len(player) == 0:
         return 0
     hits = player[21:23]
     if hits[1] == '-':
         hits = hits[0]
     return int(hits) * POINTS_PER_HIT
 
+
 def compute_fantasy_score(player: str) -> float:
+    """Return the fantasy score of player. If player is an
+    empty string, return 0.
+
+    Precondition: player is in the string of player stats as seen in players.txt
+
+    >>> compute_fantasy_score('NSH_PF_G7-_A14_DC20_H73_Pr10')
+    76.25
+    >>> compute_fantasy_score('MGO_PD_G0-_A14_DC43_H70_Pr5-')
+    53.5
+    >>> compute_fantasy_score('CLA_PG_GAA2.23_SV0.910_Pr20')
+    68.7
+    >>> compute_fantasy_score('')
+    0.0
+
+    """
     if len(player) == 0:
         return 0.0
-    
-    if get_position(player) == 'G':
+
+    if get_position(player) == GOALIE:
         gaa = player[10:14]
         sv = player[17:22]
-        return (sv * SV_VALUE) - (gaa * GAA_VALUE)
-    
-    return compute_assist_points(player) + compute_goal_points(player) + compute_dc_points(player) + compute_hit_points(player)
+        return (float(sv) * SV_VALUE) - (float(gaa) * GAA_VALUE)
+
+    score = compute_assist_points(player)
+    score += compute_goal_points(player)
+    score += compute_dc_points(player)
+    score += compute_hit_points(player)
+    return score
 
 
 if __name__ == "__main__":
--- a1_starter/test.py
+++
@@ -1,7 +0,0 @@
-
-def x(player):
-    gaa = player[10:14]
-    sv =  player[17:22]
-
-    print(gaa, sv)
-x("CLA_PG_GAA2.23_SV0.910_Pr20")
