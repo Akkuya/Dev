3ffbbf4e86b692f20283959219203070bd6dc8f4
Rishi <satyajitdatta03@gmail.com>
2026-01-06 01:11:38 -0500

    a48 a1

---
+++ School/CSCA48/hello_world_1012033336.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello! My name is: Rishi\n");
+    printf("My favorite music band is: Paramore\n");
+    printf("My favorite song from this band is: Misery Business\n");
+    printf("My goal for this term is: To get a 4.0 GPA\n");
+    printf("What I will do to relax when the term gets stressful: Sleep and listen to music\n");
+    printf("Oh yeah! I almost forgot: HELLO WORLD! (I can write programs in C now!)\n");
+    return 0;
+}
