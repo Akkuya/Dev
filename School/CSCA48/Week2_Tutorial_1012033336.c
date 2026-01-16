48fc381c54d7b9bee70c9cb5e770e3b39e0e6223
Rishi <satyajitdatta03@gmail.com>
2026-01-16 12:02:31 -0500

    a48 tut2

--- School/CSCA48/Week2_Tutorial_1012033336.c
+++ School/CSCA48/Week2_Tutorial_1012033336.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <math.h>
 
-int main() {
-  int x;
-  printf("%d\n", x);
-  return 0;
+int main()
+{
+    for (double i = 0; i < 2 * 6.18; i += 0.5)
+    {
+        float x = sin(i);
+        int y = (x + 1) * 10; // Makes sin value positive, then scales accordingly
+
+        for (int j = 0; j < y; j++)
+        {
+            printf(" ");
+        }
+        printf("*\n");
+    }
+    return 0;
 }
-
---
+++ School/CSCA48/Week2_Tutorial_Notes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+  // int x;
+  // printf("%d\n", x); // Unexpected output, prints whatever was in memory
+
+  // x = 5.56;
+  // printf("%d\n", x); // Truncates the integer: DOES NOT ROUND;
+
+  // int a = 2;
+  // int b = 1;
+
+  // if (a & b)
+  // {
+  //   printf("HELLO\n");
+  // }
+  // else
+  // {
+  //   printf("WORLD\n");
+  // } // Prints WORLD
+
+  // // ANY NON-ZERO CHARACTER IS TREATED AS TRUE
+
+  // /*
+  //   For loops can be iterated with
+
+  //   ints, floats, AND chars
+  // */
+  // return 0;
+
+  /*
+  Write a program that prints out a graph of sin(x) where x is in [0,2pi] the graph is printed to the terminla using * to indicate points that are on the graph*/
+  
+}
