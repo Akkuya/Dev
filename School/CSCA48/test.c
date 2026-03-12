c702a6abaaa88f503545238bb400a1bf7cea907b
Rishi <satyajitdatta03@gmail.com>
2026-03-12 02:12:50 -0400

    a2

--- School/CSCA48/test.c
+++ School/CSCA48/test.c
@@ -1,25 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct customer
-{
-    int id;
-    char name[256];
-} Customer;
-void set_id(Customer *p, int id) {
-    p->id = id;
-}
-void set_name(Customer *p, const char *name)  {
-    strcpy(p->name, name);
-}
-void print_customer(Customer *p) {
-    printf("ID: %d, \t Name: %s", p->id, p->name);
-}
 int main()
 {
-    Customer c1;
-    set_id(&c1, 100);
-    set_name(&c1, "Harry Potter");
-    print_customer(&c1);
-    return 0;
+    printf("%d", ((-11 % 255 + 255)%255));
 }
--- School/CSCA48/week7_tutorial.101203336.c
+++ School/CSCA48/week7_tutorial.101203336.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <math.h>
 
-#define N 1000
+#define N 100
 
 // CSCA48 - Tutorial Exercise: Testing a program
 //
@@ -26,10 +26,13 @@ void findPrimes(int array[])
     int idx = 0;
     int num = 2;
     int prime;
-    while (idx < N)
+
+    while (idx < 100) // ISSUE: N is already defined, shouldn't hardcode 100.
     {
         prime = 1;
-        for (int i = 0; i < idx; i++)
+        for (int i = 0; i <= idx; i++) // ISSUE: Index out of bounds because of <=, and not <.
+                                       // Additionally, this breaks the first iteration, which in turn breaks the whole program, 
+                                       // Since all negative numbers are divisible by positive numbers by the modulo operator.
         {
             if (num % array[i] == 0)
                 prime = 0;
@@ -43,29 +46,47 @@ void findPrimes(int array[])
     }
 }
 
+int lastDigit(int n)
+{
+    // This function returns the last (rightmost) digit in
+    // input number n - e.g. if the number is 25147, it returns 7
+
+    char number[100];
+    char lastDigit;
+
+    sprintf(number, "%d", n);
+    lastDigit = number[strlen(number) - 1]; // ISSUE: The length of number is 100, and there is no null terminator,
+                                            // So the program is taking a garbage value.
+
+                                            // A simple: return n % 10 would suffice.
+    return lastDigit;
+}
+
 void countDigits(int counts[], int array[])
 {
     // This function counts how many times each
     // digit appears as the final digit in one of
     // the first N primes
     for (int i = 0; i < N; i++)
-        counts[array[i] % 10]++;
+        counts[lastDigit(array[i])]++;
 }
 
 int main()
 {
     int primesArray[N];
     int digitCounts[10];
-    for (int i = 0; i < 10; i++)
-        digitCounts[i] = 0;
+
     for (int i = 0; i < N; i++)
         primesArray[i] = -1; // Mark this as unassigned
- 
+    
+    // ISSUE: Digitcounts is not cleared, currently garbage.
     findPrimes(primesArray);
+
     // Count the last digits and print the counts!
     countDigits(digitCounts, primesArray);
 
     for (int i = 0; i < 10; i++)
         printf("Digit %d appears %d times as the last digit in these primes\n", i, digitCounts[i]);
-    return 0;
+
+    // No return value for main.
 }
